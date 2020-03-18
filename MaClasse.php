4f6f4da235486849eec2996758d18ee3d7b6c9cd
talibadio <jackweb178@gmail.com>
2020-03-18 14:57:31 +0000

    Voir si lattribut au quel on essaye d'acceder existe!

--- MaClasse.php
+++ MaClasse.php
@@ -6,23 +6,28 @@ class MaClasse
     private $unAttributPrive;
     private $TabAttribut=[];
 
+    //https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1666950-les-methodes-magiques#/id/r-1670145
+    public  function __get($name)
+    {
+        if (isset($this->TabAttribut[$name]))
+        {
+            return $this->TabAttribut[$name];
+        }else
+        {
+            echo '<br/>==', $name,"== Cet attribut n'existe pas ";
+        }
+    }
+
     //https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1666950-les-methodes-magiques#/id/r-1670134
     public function __set($name, $value)
     {
-        //echo "lattribut est <strong>", $name, "</strong> et la valeur est :  <strong>", $value, " </strong><br>";
         $this->TabAttribut[$name]=$value;
     }
     public function afficher()
     {
-        echo '<pre>', print_r($this->TabAttribut) ,'</pre>';
-    }
+        echo '<pre>', print_r($this->TabAttribut,true) ,'</pre>';
 
-    //https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1666950-les-methodes-magiques#/id/r-1670145
-    public  function __get($name)
-    {
-        return "Impossible dacceder a lattribut <strong> $name </strong> ,desole !<br/>";
     }
-
 }
 $set= new MaClasse();
 
@@ -30,7 +35,6 @@ $set ->attribut='Simple test';
 $set->unAttributPrive = 'Autre simple test';
 $set->afficher();
 
-$get=new MaClasse;
+echo $set->attribut;
+echo $set->autreAttribut;
 
-echo $get->attribut;
-echo $get->unAttributPrive;
